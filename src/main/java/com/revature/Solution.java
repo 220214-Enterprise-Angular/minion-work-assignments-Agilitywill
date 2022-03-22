9fb292f549351ba37b5c49b7a6a5db53e8b54019
gearyw68 <47993243+gearyw68@users.noreply.github.com>
2022-03-22 17:31:13 -0400

    Update Solution.java

--- src/main/java/com/revature/Solution.java
+++ src/main/java/com/revature/Solution.java
@@ -1,5 +1,10 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution {
 
 	/**
@@ -10,8 +15,50 @@ public class Solution {
 	
 	public static int[] solution(int[] data, int n) {
 		
+		// create a new ArrayList object 
+		List<Integer> a = new ArrayList<>(); 
 		
-		return null;
-	}
+		if (n < 1) {
+			return new int[0]; 
+			
+		} else if (data.length < 1)  {
 
+			return data; 
+		}
+				
+		for (int i = a.size() - 1; i > -1; i--) {
+			
+			ArrayList<Integer> t = new ArrayList<>(); 
+			
+			for (int j = 0; j < a.size(); j++) {
+				
+				if(a.get(j) == a.get(i)) {
+					t.add(j); 
+				}
+				
+			}
+			
+			if(t.size() > n){
+	            for(int j = t.size()-1; j > -1; j--){
+	                a.remove((int) t.get(j));
+	            }
+
+	            i -= t.size()-1;
+	        }
+	    }
+
+	    data = new int[a.size()];
+	    int c = 0;
+	    
+	    for(int d : a){
+	        data[c] = d;
+	        c++;
+	    }
+
+	    return data;
+	}
+	
 }
+
+
+
